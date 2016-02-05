9f9e04e0780c1dfe1ab7d1676cebd218d0ce6cef
RandomlyKnighted <thughes3@jsu.edu>
2016-02-04 22:07:30 -0600

    Updated package.json

--- gruntfile.js
+++ gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function( grunt ) {
         travis: {
             configFile: "karma.conf.js",
             singleRun: true,
-            browswer: ["PhantomJS"]
+            browser: ["PhantomJS"]
         }
     },
     uglify: {
