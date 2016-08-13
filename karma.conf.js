8b3306d5f0370e898a9b237a349181bc5093627f
Noah H. Smith <noah@batteredoats.com>
2016-08-12 17:55:02 -0700

    setting up the project

--- karma.conf.js
+++ karma.conf.js
@@ -1,64 +1,46 @@
-// Karma configuration
-// Generated on Fri Jan 29 2016 21:36:15 GMT-0600 (Central Standard Time)
-
-module.exports = function(config) {
+/* eslint-env node */
+module.exports = function (config) {
   config.set({
+    browsers: ['PhantomJS'],
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: "",
+    frameworks: ['browserify', 'mocha', 'chai', 'sinon'],
 
+    reporters: ['mocha'],
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ["mocha", "expect", "sinon"],
-
-
-    // list of files / patterns to load in the browser
     files: [
-      "lib/**/*.js",
-      "test/**/*.js"
+      '**/*.js'
     ],
 
-
-    // list of files to exclude
     exclude: [
+      'node_modules/**/*.js'
     ],
 
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      '**/*.js': ['eslint'],
+      'test/**/test-*.js': ['browserify']
     },
 
+    eslint: {
+      stopOnWarning: false,
+      stopOnError: false
+    },
 
-    // web server port
-    port: 9876,
+    browserify: {
+      debug: true,
+      transform: [
+        ['babelify']
+      ]
+    }
+  });
 
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [ "PhantomJS" ],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
-}
+  if (process.env.CIRCLECI) {
+    config.set({
+      reporters: ['progress', 'junit'],
+      junitReporter: {
+        outputDir: process.env.CIRCLE_TEST_REPORTS + '/junit/',
+        outputFile: 'test-results.xml',
+        useBrowserName: false
+      }
+    });
+  }
+};
