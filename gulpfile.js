8d835d6d54b668f46cb01c3539e85d3655788271
Pallas <tyler@pallas.media>
2020-08-29 21:46:13 -0500

    Formatted file

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 /* eslint-env node */
-var _ = require('lodash');
-var gulp = require('gulp');
-var Server = require('karma').Server;
+var _ = require( 'lodash' );
+var gulp = require( 'gulp' );
+var Server = require( 'karma' ).Server;
 
-function karmaServer(config) {
+function karmaServer( config ) {
   var configFile = __dirname + '/karma.conf.js';
-  new Server(_.defaults(config || {}, {configFile})).start();
+  new Server( _.defaults( config || {}, { configFile } ) ).start();
 }
 
-gulp.task('test', () => karmaServer({
+gulp.task( 'test', () => karmaServer( {
   autoWatch: false,
   singleRun: true
-}));
+} ) );
 
-gulp.task('tdd', () => karmaServer());
+gulp.task( 'tdd', () => karmaServer() );
